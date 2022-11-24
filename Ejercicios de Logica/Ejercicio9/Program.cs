784e0fa86835aa0d7c197a18e8cd2ec58378860d
Alexander Meneses <118413232+alexmnt710@users.noreply.github.com>
2022-11-23 20:10:08 -0500

    Add files via upload

--- Ejercicios de Logica/Ejercicio9/Program.cs
+++ Ejercicios de Logica/Ejercicio9/Program.cs
@@ -1,16 +1,9 @@
-﻿internal partial class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        int contador=0, numero1, total=0;
-        do{
-            contador++;
-            Console.WriteLine("Ingrese un numero por favor (Si ingresa un 0, programa terminara)");
-            numero1= int.Parse(Console.ReadLine());
-            total=total+numero1;
-
-        }while(numero1!=0);
-        total=total/contador;
-        Console.WriteLine("La media aritmetica de los numeros ingresados fueron: "+total);
+        Console.WriteLine("Multiplique dos variables asignadas por 5 veces");
+        proceso proceso1 = new proceso ();
+        proceso1.calculo ();
     }
 }
