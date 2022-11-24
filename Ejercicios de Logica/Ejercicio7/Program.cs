cba3e179c6001e193efaf3d777a1fab460831140
Alexander Meneses <118413232+alexmnt710@users.noreply.github.com>
2022-11-23 20:11:04 -0500

    Add files via upload

--- Ejercicios de Logica/Ejercicio7/Program.cs
+++ Ejercicios de Logica/Ejercicio7/Program.cs
@@ -1,34 +1,22 @@
-﻿internal partial class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        int lado1, lado2, lado3;
-        string respuesta;
+        string confirmacion;
+        Console.WriteLine("Programa para identificar un tipo de triangulo conociendo sus longitudes");
+        proceso proceso1 = new proceso(0,0,0);
+
         do{
-            Console.WriteLine("Ingrese tres numeros por favor");
-
-            Console.WriteLine("Ingrese el primero por favor");
-            lado1= int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Ingrese el segundo por favor");
-            lado2= int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Ingrese el tercero por favor");
-            lado3= int.Parse(Console.ReadLine());
-
-            if((lado1 == lado2 && lado1!=lado3)||(lado2==lado3&&lado2!=lado1)||(lado3==lado1&&lado3!=lado2)){
-                Console.WriteLine("Los lados forman un triangulo isosceles");
-            }
-            if(lado1==lado2 && lado1==lado3){
-                Console.WriteLine("Los lados forman un triangulo equilatero");
-            }
-            if(lado1 != lado2 && lado1!= lado3 && lado2!= lado3){
-                Console.WriteLine("Los lados forman un triangulo escaleno");
-            }
-
-            Console.WriteLine("Quieres volver a hacerlo?(S para si y N para no)");
-            respuesta = Console.ReadLine().ToUpper();
-
-        }while(respuesta!="N");
+        Console.WriteLine("agrege el longitud lado uno");
+        proceso1.lado1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("agrege el longitud lado dos");
+        proceso1.lado2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("agrege el longitud lado tres");
+        proceso1.lado3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine (proceso1.identificar());
+        Console.WriteLine ("Desea continuar, escriba Si o NO");
+        confirmacion = Console.ReadLine(); 
+        }while(confirmacion == "Si");
+        Console.WriteLine ("El programa a terminado");
     }
 }
